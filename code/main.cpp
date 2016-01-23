1c558a73787b8bb6b5b37d23f7e95ea03fe1e2a6
Patryk Wertka <spartanpageagain@gmail.com>
2016-01-23 15:51:06 +0100

    Added frames counter label;
    Adjusted canvas and world size for better performance.

--- code/main.cpp
+++ code/main.cpp
@@ -9,17 +9,27 @@
 #include "game/entity/player/player.hpp"
 #include "game/entity/actor/manager/actors-manager.hpp"
 
+#include "console/ui/core.hpp"
+#include "console/ui/label/label.hpp"
+
+#include <string>
+#include <chrono>
+using namespace std;
+
 using console::graphics::color;
 using rebubox::main_loop::frame_t;
 
 int main(){
     console::set_cursor_visibility(false);
 
-    console_canvas canvas(119, 29);
+    console_canvas canvas(85, 15);
     console_drawer canvas_drawer(canvas);
 
-    rebubox::world world(60, 29);
+    rebubox::world world(60, 15);
     rebubox::world_drawer world_drawer(canvas, world);
+
+    console::ui::core ui_core;
+    ui_core.add_element("frames_counter", new console::ui::label(canvas, "", { 60, 0 }, 25, color::cyan, color::dark_yellow));
 
     rebubox::entity::actors_manager actors;
     actors.add_actor(new rebubox::entity::player(canvas, { 2, 2 }));
@@ -41,6 +51,7 @@ int main(){
     auto update = [&](frame_t){
         actors.update_all();
 
+        ui_core.update();
         //consume_keys();
     };
 
@@ -48,9 +59,16 @@ int main(){
         world_drawer.draw();
         actors.draw_all();
 
-        frame == 1
-            ? canvas_drawer.draw()
-            : canvas_drawer.update_drawing();
+        ui_core.access_element_by_tag("frames_counter").as<console::ui::label>()
+            .set_text("frames counter: " + to_string(frame));
+        ui_core.draw();
+
+        if(frame == 1){
+            canvas_drawer.draw();
+        }
+        else{
+            canvas_drawer.update_drawing();
+        }
     };
 
     auto end_game = [](frame_t){

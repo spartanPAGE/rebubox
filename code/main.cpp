d3fcfcd31df25f92b13525da843511b1eb89710c
Patryk Wertka <spartanpageagain@gmail.com>
2016-01-23 14:34:16 +0100

    Adjusted sample main to game-loop reqs

--- code/main.cpp
+++ code/main.cpp
@@ -1,10 +1,15 @@
 #include "console/canvas/console-canvas.hpp"
 #include "console/canvas/console-drawer.hpp"
 
+#include "console/input-utils/input-utils.hpp"
+
 #include "game/world/world.hpp"
 #include "game/world/drawer/world-drawer.hpp"
 
+#include "game/main-loop/main-loop.hpp"
 using console::graphics::color;
+using rebubox::main_loop::frame_t;
+
 int main(){
     console_canvas canvas(119, 29);
     console_drawer canvas_drawer(canvas);
@@ -21,7 +26,23 @@ int main(){
         }
     }
 
-    world_drawer.draw();
-    canvas_drawer.draw();
+    auto update = [&](frame_t){};
+    auto draw = [&](frame_t frame){
+        world_drawer.draw();
+
+        frame == 1
+            ? canvas_drawer.draw()
+            : canvas_drawer.update_drawing();
+    };
+
+    auto end_game = [](frame_t){
+        char esc = 27;
+        if(console::key_pressed()){
+            return console::get_key() == esc;
+        }
+    };
+
+    rebubox::start_main_loop(update, draw, end_game);
+
     console::set_color(color::white, color::black);
 }
